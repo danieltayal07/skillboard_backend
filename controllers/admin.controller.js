5d40cf74d142f5e35451d3340adf8e414109790c
danieltayal07 <tayaldaniel2@gmail.com>
2025-12-04 11:48:03 +0530

    reduced number of page limit to test pagination

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -3,7 +3,7 @@ const prisma = require("../config/prisma");
 const getAllUsers = async (req, res) => {
   try {
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 2;
     const skip = (page - 1) * limit;
 
     const [users, total] = await Promise.all([
